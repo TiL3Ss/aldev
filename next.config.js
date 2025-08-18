7b6f78e8e2b94f2b48f7e6ea90c78ab6688ae2f3
Alvaro Chavez Melo <1.tiless@gmail.com>
2025-08-18 19:18:17 -0300

    fix postcss config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: true
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
