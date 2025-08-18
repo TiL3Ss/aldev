d2013e3f993c7b76a2087d09cf5895327d8d5f75
Alvaro Chavez Melo <1.tiless@gmail.com>
2025-08-18 19:10:07 -0300

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,25 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
-import { Inter } from 'next/font/google';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Aldev',
-    default: 'Portafolio Aldev',
+    template: "%s - Aldev",
+    default: "Portafolio Aldev",
   },
-  description: 'Portafolio de Aldev, un desarrollador web full stack',
-  viewport: 'width=device-width, initial-scale=1',
+  description: "Portafolio de Aldev, un desarrollador web full stack",
+  viewport: "width=device-width, initial-scale=1",
   icons: {
-    icon: '/favicon.ico',
+    icon: "/favicon.ico",
   },
-}
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const dynamic = 'force-static';
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <body>
-            {children}
+    <html lang="es" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className={GeistSans.className}>
+        {children}
       </body>
     </html>
   );
